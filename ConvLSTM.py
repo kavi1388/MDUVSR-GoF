8e1aa9eed789b5a5bed3cd3e7e21b504758f0f04
kavi1388 <47577144+kavi1388@users.noreply.github.com>
2023-01-06 19:57:13 +0530

    Updates

--- ConvLSTM.py
+++ ConvLSTM.py
@@ -9,24 +9,24 @@ import torch.nn as nn
 # Original ConvLSTM cell as proposed by Shi et al.
 class ConvLSTMCell(nn.Module):
 
-    def __init__(self, in_channels, out_channels, 
+    def __init__(self, in_channels, out_channels,
     kernel_size, padding, activation, frame_size):
 
-        super(ConvLSTMCell, self).__init__()  
-        
+        super(ConvLSTMCell, self).__init__()
+
 #         print(f'in_channels, out_channels, kernel_size, padding, activation, frame_size {in_channels, out_channels,kernel_size, padding, activation, frame_size}')
 
         if activation == "tanh":
-            self.activation = torch.tanh 
+            self.activation = torch.tanh
         elif activation == "relu":
             self.activation = torch.relu
-        
+
         # Idea adapted from https://github.com/ndrplz/ConvLSTM_pytorch
         self.conv = nn.Conv2d(
-            in_channels=in_channels + out_channels, 
-            out_channels=4 * out_channels, 
-            kernel_size=kernel_size, 
-            padding=padding)           
+            in_channels=in_channels + out_channels,
+            out_channels=4 * out_channels,
+            kernel_size=kernel_size,
+            padding=padding)
 
         # Initialize weights for Hadamard Products
         self.W_ci = nn.Parameter(torch.Tensor(out_channels, *frame_size))
@@ -34,21 +34,21 @@ class ConvLSTMCell(nn.Module):
         self.W_cf = nn.Parameter(torch.Tensor(out_channels, *frame_size))
 
     def forward(self, X, H_prev, C_prev):
-        
+
 #         print(f'H_prev = {H_prev.shape}')
 #         print(f'X = {X.shape}')
 
         # Idea adapted from https://github.com/ndrplz/ConvLSTM_pytorch
         conv_output = self.conv(torch.cat([X, H_prev], dim=1))
-        
+
 #         print(f'conv_output {conv_output.shape}')
 
         # Idea adapted from https://github.com/ndrplz/ConvLSTM_pytorch
         i_conv, f_conv, C_conv, o_conv = torch.chunk(conv_output, chunks=4, dim=1)
-        
+
 #         print(f'i_conv = {i_conv.shape}')
 #         print(f'f_conv = {f_conv.shape}')
-        
+
 #         print(f'self.W_ci = {self.W_ci.shape}')
 #         print(f'self.W_cf = {self.W_cf.shape}')
 #         print(f'C_prev = {C_prev.shape}')
@@ -68,7 +68,7 @@ class ConvLSTMCell(nn.Module):
 
 class ConvLSTM(nn.Module):
 
-    def __init__(self, in_channels, out_channels, 
+    def __init__(self, in_channels, out_channels,
     kernel_size, padding, activation, frame_size):
 
         super(ConvLSTM, self).__init__()
@@ -76,7 +76,7 @@ class ConvLSTM(nn.Module):
         self.out_channels = out_channels
 
         # We will unroll this over time steps
-        self.convLSTMcell = ConvLSTMCell(in_channels, out_channels, 
+        self.convLSTMcell = ConvLSTMCell(in_channels, out_channels,
         kernel_size, padding, activation, frame_size)
 
     def forward(self, X):
@@ -89,15 +89,14 @@ class ConvLSTM(nn.Module):
         # Initialize output
         output = torch.zeros(batch_size, seq_len,  self.out_channels,
         height, width, device=device)
-        
+
         # Initialize Hidden State
-        H = torch.zeros(batch_size, self.out_channels, 
+        H = torch.rand(batch_size, self.out_channels,
         height, width, device=device)
 
         # Initialize Cell Input
-        C = torch.zeros(batch_size,self.out_channels, 
+        C = torch.rand(batch_size,self.out_channels,
         height, width, device=device)
-        
 
         # Unroll over time steps
         for time_step in range(seq_len):
