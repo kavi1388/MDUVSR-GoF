0139a9f2932f9fb8724a4ddd02c7452f036f650d
kavi1388 <47577144+kavi1388@users.noreply.github.com>
2023-01-16 17:41:09 +0530

    Updates

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import argparse
 import cv2
-
+from piqa import SSIM
 
 # Initialize parser
 parser = argparse.ArgumentParser()
@@ -114,49 +114,52 @@ print(summary(model, (train_loader.dataset[0][0].shape)))
 
 """### Loss Function"""
 
+#
+# def get_outnorm(x: torch.Tensor, out_norm: str = '') -> torch.Tensor:
+#     """ Common function to get a loss normalization value. Can
+#         normalize by either the batch size ('b'), the number of
+#         channels ('c'), the image size ('i') or combinations
+#         ('bi', 'bci', etc)
+#     """
+#     # b, c, h, w = x.size()
+#     img_shape = x.shape
+#
+#     if not out_norm:
+#         return 1
+#
+#     norm = 1
+#     if 'b' in out_norm:
+#         # normalize by batch size
+#         # norm /= b
+#         norm /= img_shape[0]
+#     if 'c' in out_norm:
+#         # normalize by the number of channels
+#         # norm /= c
+#         norm /= img_shape[-3]
+#     if 'i' in out_norm:
+#         # normalize by image/map size
+#         # norm /= h*w
+#         norm /= img_shape[-1] * img_shape[-2]
+#
+#     return norm
+#
+#
+# class CharbonnierLoss(nn.Module):
+#     """Charbonnier Loss (L1)"""
+#
+#     def __init__(self, eps=1e-6, out_norm: str = 'bci'):
+#         super(CharbonnierLoss, self).__init__()
+#         self.eps = eps
+#         self.out_norm = out_norm
+#
+#     def forward(self, x, y):
+#         norm = get_outnorm(x, self.out_norm)
+#         loss = torch.sum(torch.sqrt((x - y).pow(2) + self.eps ** 2))
+#         return loss * norm
 
-def get_outnorm(x: torch.Tensor, out_norm: str = '') -> torch.Tensor:
-    """ Common function to get a loss normalization value. Can
-        normalize by either the batch size ('b'), the number of
-        channels ('c'), the image size ('i') or combinations
-        ('bi', 'bci', etc)
-    """
-    # b, c, h, w = x.size()
-    img_shape = x.shape
-
-    if not out_norm:
-        return 1
-
-    norm = 1
-    if 'b' in out_norm:
-        # normalize by batch size
-        # norm /= b
-        norm /= img_shape[0]
-    if 'c' in out_norm:
-        # normalize by the number of channels
-        # norm /= c
-        norm /= img_shape[-3]
-    if 'i' in out_norm:
-        # normalize by image/map size
-        # norm /= h*w
-        norm /= img_shape[-1] * img_shape[-2]
-
-    return norm
-
-
-class CharbonnierLoss(nn.Module):
-    """Charbonnier Loss (L1)"""
-
-    def __init__(self, eps=1e-6, out_norm: str = 'bci'):
-        super(CharbonnierLoss, self).__init__()
-        self.eps = eps
-        self.out_norm = out_norm
-
+class SSIMLoss(SSIM):
     def forward(self, x, y):
-        norm = get_outnorm(x, self.out_norm)
-        loss = torch.sum(torch.sqrt((x - y).pow(2) + self.eps ** 2))
-        return loss * norm
-
+        return 1. - super().forward(x / 255, y / 255)
 
 """### Training"""
 
@@ -164,7 +167,8 @@ scaler = torch.cuda.amp.GradScaler()
 optimizer = optim.AdamW(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False)
 
 # criterion = nn.L1Loss()
-criterion = CharbonnierLoss()
+# criterion = CharbonnierLoss
+criterion = SSIMLoss().cuda()
 num_epochs = epochs
 
 """### Training"""
