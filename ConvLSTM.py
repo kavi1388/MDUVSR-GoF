fed0bd03e704ac84eeabafe820c5a7994c0a9754
kavi1388 <47577144+kavi1388@users.noreply.github.com>
2023-01-16 14:39:35 +0530

    Updates

--- ConvLSTM.py
+++ ConvLSTM.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Original ConvLSTM cell as proposed by Shi et al.
 class ConvLSTMCell(nn.Module):
 
